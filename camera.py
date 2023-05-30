7f8d7506712c3ecb86f2ce621ea577bec258a040
Ameli98 <rain0210303@gmail.com>
2023-05-30 20:27:04 +0800

    feat: store and read qrcode data as json

--- camera.py
+++ camera.py
@@ -4,19 +4,22 @@ import cv2
 cap = cv2.VideoCapture(0)
 qrcode = cv2.QRCodeDetector()
 
+# open json file
+output_file = open("data.json", "w")
+
 while(True):
     # fetch an frame from camera
     ret, frame = cap.read()
 
     # Show the frame
     cv2.imshow('frame', frame)
-
+    cv2.waitKey(1)
     # scan the frame for qrcode
     data, bbox, rectified = qrcode.detectAndDecode(frame)
     if data != "":
-        print(data)
+        print(data, file=output_file)
         break
-    
+
     # press 'q' to exit
     # if cv2.waitKey(1) & 0xFF == ord('q'):
     #     break
--- upload.py
+++ upload.py
@@ -1,22 +1,24 @@
 import iota_client
 from pprint import pprint
-import json
 
 
 client = iota_client.Client()
 
+f = open("data.json", "r")
+
 # encoding utf string into list of bytes
-data = {
-    'name': 'Bible',
-    'status': 'borrow',        # borrow / return
-    'user': 'Amelia Huang',
-    'from': 'NTU_liabrary',
-    'date': '1/1/2023',
-    'due': '3/1/2023',
-    }
+data = f.readline()
+# data = {
+#     "name": "Bible",
+#     "status": "borrow",        # borrow / return
+#     "user": "Amelia Huang",
+#     "from": "NTU_liabrary",
+#     "date": "1/1/2023",
+#     "due": "3/1/2023"
+#     }
 # data = "some utf based data".encode("utf8")
 
 message = client.message(
-    index="some_data_indey", data=str(data).encode("utf8")
+    index="some_data_index", data=str(data).encode("utf8")
 )
 pprint(message)
