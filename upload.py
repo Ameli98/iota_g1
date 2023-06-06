8899c2150ce0779b27b3c37d256a3f2cbe98b0b1
Ameli98 <rain0210303@gmail.com>
2023-06-06 21:12:15 +0800

    feat: synchronize the message_id

--- upload.py
+++ upload.py
@@ -1,13 +1,13 @@
-import iota_client
-from pprint import pprint
+import iota_client, json, subprocess, os
+from dotenv import load_dotenv
 
 def upload():
     client = iota_client.Client()
 
-    f = open("data.json", "r")
-
+    with open("data.json", "r") as f:
+        data = json.load(f)
+        name = data["name"]
     # encoding utf string into list of bytes
-    data = f.readline()
     # data = {
     #     "name": "Bible",
     #     "status": "borrow",        # borrow / return
@@ -21,7 +21,25 @@ def upload():
     message = client.message(
         index="some_data_index", data=str(data).encode("utf8")
     )
-    pprint(message)
+    id = message["message_id"]
 
+    # Synchronize message_id.json from the database
+    load_dotenv()
+    destination = os.getenv("DESTINATION")
+    subprocess.run(["scp", destination, "message_id.json"])
+
+    # Record message_id in message_id.json
+    with open("message_id.json", "r") as m:
+        try:
+            id_set = json.load(m)
+        except json.decoder.JSONDecodeError:
+            id_set = {}
+        id_set[name] = id
+
+    with open("message_id.json", "w") as m:
+        json.dump(id_set, m)
+
+    # Update the latest message_id.json to the database
+    subprocess.run(["scp", "message_id.json", destination])
 if __name__ == "__main__":
     upload()
