23a1626ea13de19d7f4368ba7f0732ece5b6410b
Ameli98 <rain0210303@gmail.com>
2023-06-14 15:53:15 +0800

    fear: login by address

--- main.py
+++ main.py
@@ -5,35 +5,67 @@ import os
 from getpass import getpass
 import argparse
 import json
+import iota_wallet as iw
+from dotenv import load_dotenv
 
-# the borrow/lend process
+# Load the env variables｀
+load_dotenv()
+# Get the stronghold password
+STRONGHOLD_PASSWORD = os.getenv('SH_PASSWORD')
+# This example checks the account balance.
+account_manager = iw.AccountManager(
+    storage_path="./library_database"
+)
+account_manager.set_stronghold_password(STRONGHOLD_PASSWORD)
 
 
-def main(username, overdue=False):
-    scan(username,overdue)
+def main(username=None, overdue=False):
+    scan(username, overdue)
     if pay(username):
         upload()
-        
+
 
 if __name__ == "__main__":
     # option build
     parser = argparse.ArgumentParser()
-    parser.add_argument("user_name", type=str, help="your username")
+    parser.add_argument("-un", "--user_name", type=str, nargs="?", help="your username")
     parser.add_argument("-bn", "--book_number", nargs="?", type=int,
                         default=1, help="number of books you want to borrow or return")
-    parser.add_argument("-od", "--overdue_demo", action="store_true", help="Demo of overdueing")
+    parser.add_argument("-od", "--overdue_demo",
+                        action="store_true", help="Demo of overdueing")
 
     args = parser.parse_args()
 
     # load right password from database
     with open("account.json", "r") as acc:
         account_set = json.load(acc)
-    username = args.user_name
-    try:
-        right_password = account_set[username]
-    except KeyError:
-        print("No such user exists, session terminated.")
-        os._exit(1)
+    if not args.user_name:
+        address = input("Your address: ")
+        for user in account_set.keys():
+            account = account_manager.get_account(user)
+
+            address_list = []
+            for i in range(4):
+                address_list.append(account.addresses()[i]['address']['inner'])
+
+            if address in address_list:
+                # get a specific instance of some account
+                account = account_manager.get_account(user)
+                print(f'User {account.alias()} login')
+                user_exist = True
+                username = user
+                break
+        if not user_exist:
+            print("User is not exist. Process terminated.")
+            os._exit(1)
+    else:
+        username = args.user_name
+        try:
+            right_password = account_set[username]
+            user_exist = True
+        except KeyError:
+            print("No such user exists, session terminated.")
+            os._exit(1)
 
     # login
     attempt = 0
